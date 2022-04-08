c913f552ef602803de83b5279a7813f472a5a89d
Blakelikestoast <collegeblake11@gmail.com>
2022-04-08 15:45:38 -0700

    Updated .js File

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Password-Creator
 This is a password generator you can use to create a secure password your information!
+
+I started by creating strings of possible characters.
+I then created the windows that collected user input regarding upper and lowercase letters, numbers, and symbols. 
+Next, I made the function that combined all of those strings based off the user input. If true, the next string is added, if false, the string is omitted.
+Then, I made the generate password function that randomly selects the specific number of characters asked for from the finished string.
+Lastly I changed the passwordtext.value to match the output of my generate password function.
+
+[Deployed Application](https://blakelikestoast.github.io/Password-Creator/)
+![Web Page](Images/PasswordGenerator.png)
--- script.js
+++ script.js
@@ -6,9 +6,70 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = pword;
+  console.log(pword.length)
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+/* Global Vairiables */
+let Uppercase_Pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let Lowercase_Pool = "abcdefgvhijklmnopqrstuvwxyz";
+let Symbols_Pool =  "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+let Numbers_Pool = "0123456789";
+
+/* Collecting password parameters */
+const Length = Number(window.prompt("Enter a number between 8 and 128"));
+const Upper = window.confirm("Would you like uppercase letters?");
+const Symbol = window.confirm("Would you like symbols?")
+const Numbers = window.confirm("Would you like numbers?")
+
+// Creates Pool of characters based off password parameters
+let Pool = null;
+function createPool(){
+  if(Length >=8 && Length <=128){
+    Pool = Lowercase_Pool;
+    console.log("Length", Pool);
+  } else {
+    window.alert("Invalid Entry", Pool);
+  } 
+  if(Upper){
+    Pool = Lowercase_Pool.concat(Uppercase_Pool);
+    console.log("Upper", Pool);
+  } else {
+    console.log("No Uppercase", Pool);
+  }
+  if(Symbol){
+    Pool = Pool.concat(Symbols_Pool);
+    console.log("Symbol", Pool);
+  } else {
+    console.log("No Symbols", Pool);
+  }
+  if(Numbers){
+    Pool = Pool.concat(Numbers_Pool);
+    console.log("Numbers", Pool);
+  } else {
+    console.log("No Numbers", Pool);
+  }
+  return Pool;
+}
+createPool();
+
+let pword = ""
+function generatePassword(){
+  console.log(Pool);
+  for (let i = 0; i < Length; i++) {
+    pword += Pool.charAt(Math.floor(Math.random() * Pool.length))
+  }
+}
+
+
+
+
+
+
+
+
